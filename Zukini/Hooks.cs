a346784b9921d28b211a6e99074486be468cf801
MikeMugu <mikemugu@gmail.com>
2015-03-25 20:12:02 -0400

    Replaces page factory with Coypu
    Signed-off-by: MikeMugu <mikemugu@gmail.com>

---
+++ Zukini/Hooks.cs
@@ -0,0 +1,58 @@
+﻿using BoDi;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Zukini
+{
+    [Binding]
+    public class Hooks
+    {
+        private IObjectContainer _objectContainer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Hooks"/> class.
+        /// </summary>
+        /// <param name="objectContainer">The object container (Injected with DI).</param>
+        public Hooks(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+        }
+
+        /// <summary>
+        /// Global BeforeScenario hook used to new up the WebDriver instance
+        /// prior to each test.
+        /// </summary>
+        [BeforeScenario]
+        protected void BeforeScenario()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            _objectContainer.RegisterInstanceAs<IWebDriver>(driver);
+        }
+
+        /// <summary>
+        /// Global After Scenario hook used to take a screenshot (if there is an error) 
+        /// and shuts down the driver.
+        /// </summary>
+        [AfterScenario]
+        protected void AfterScenario()
+        {
+            var driver = _objectContainer.Resolve<IWebDriver>();
+            if (driver != null)
+            {
+                if (ScenarioContext.Current.TestError != null)
+                {
+                    Screenshot screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+                    screenshot.SaveAsFile("TestError.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+
+                driver.Quit();
+            }
+        }
+    }
+}
---
+++ Zukini/Pages/CurrentPageException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zukini.Pages
+{
+    public class CurrentPageException 
+        : Exception
+    {
+        public CurrentPageException(string message) 
+            : base(message)
+        {
+        }
+    }
+}
