10de21af58d4972ad3cc59bdd0c273d6c7850f47
MikeMugu <mikemugu@gmail.com>
2015-03-29 12:08:23 -0400

    Adds missed Hooks.cs file in example project. Updates AssemblyInfo.cs with product version and description.

---
+++ Zukini.Examples.Features/Hooks.cs
@@ -0,0 +1,30 @@
+﻿using BoDi;
+using Coypu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Zukini.Examples.Features
+{
+    [Binding]
+    public class Hooks
+    {
+        private readonly SessionConfiguration _sessionConfiguration;
+
+        public Hooks(SessionConfiguration config)
+        {
+            _sessionConfiguration = config;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            _sessionConfiguration.Browser = Coypu.Drivers.Browser.Firefox;
+            _sessionConfiguration.Timeout = TimeSpan.FromSeconds(3);
+            _sessionConfiguration.RetryInterval = TimeSpan.FromSeconds(0.1);
+        }
+    }
+}
--- Zukini/Properties/AssemblyInfo.cs
+++ Zukini/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Zukini")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The sexy way to do test automation in .NET.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mike Watkins")]
 [assembly: AssemblyProduct("Zukini")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
