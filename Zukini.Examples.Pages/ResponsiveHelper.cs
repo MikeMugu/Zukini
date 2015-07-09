f68376450a14cc3592162064bcc6d1ae90b0f6f4
Jim McGrath <jimmcgrath@lmo.com>
2015-07-09 15:37:15 -0400

    Responsive Tests and Helpers

---
+++ Zukini.Examples.Pages/ResponsiveHelper.cs
@@ -0,0 +1,47 @@
+﻿using Coypu;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zukini.Pages;
+
+
+
+namespace Zukini.Examples.Pages
+{
+    public class ResponsiveHelper : BasePage
+    {
+
+
+
+        public ResponsiveHelper(BrowserSession browser)
+            : base(browser)
+        {
+        }
+
+
+        public void AAndBLineUp(ElementScope field, ElementScope field2)
+        {
+            string One = field["offsetTop"];
+            string Two = field2["offsetTop"];
+            int one = int.Parse(One);
+            int two = int.Parse(Two);
+            Assert.AreEqual(one, two);
+        }
+
+        public void ABeforeB(ElementScope field, ElementScope field2)
+        {
+            string One = field["offsetTop"];
+            string Two = field2["offsetTop"];
+            int one = int.Parse(One);
+            int two = int.Parse(Two);
+            int result = (two - one);
+            Assert.Less(one, two);
+        }
+
+
+
+    }
+}
---
+++ Zukini.Examples.Pages/WWFPage.cs
@@ -0,0 +1,25 @@
+﻿using Coypu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zukini.Pages;
+
+namespace Zukini.Examples.Pages
+{
+    public class WWFPage : BasePage
+    {
+        public WWFPage(BrowserSession browser)
+            : base(browser)
+        { 
+        }
+
+        public ElementScope Header { get { return Browser.FindId("header"); } }
+        public ElementScope MenuIcon { get { return Browser.FindCss(".ico.open"); } }
+
+        public ElementScope Text { get { return Browser.FindCss(".span6.gutter-top-in-4.gutter-bottom-in-2.gutter-horiz-in"); } }
+        public ElementScope Image { get { return Browser.FindCss(".span6.medium-break.gutter-top-in-3.gutter-horiz-in.gutter-bottom-in-2"); } }
+
+    }
+}
